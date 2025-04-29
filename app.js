edbb7b8c5bf9e7b59ec306e5680a72a405a099f5
i-m-rajgupta <neeleshgupta32154@gmail.com>
2025-04-29 13:47:42 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,145 @@
+
+let redDiv = document.querySelector("#red");
+let greenDiv = document.querySelector("#green");
+let yellowDiv = document.querySelector("#yellow");
+let blueDiv = document.querySelector("#blue");
+
+let level = document.querySelector(".level");
+let levelNum = 0;
+let gameMemory = [];
+let userMemory =[];
+
+let color;
+
+let isMobile = /iPhone|iPad|Android|Mobile/i.test(navigator.userAgent);
+const input = document.getElementById('hidden-input');
+// const button = document.getElementById('start-button');
+
+ if (isMobile) {
+      button.style.display = 'inline-block';
+      input.style.opacity = 1; // make input slightly visible to ensure focus
+      input.focus();
+    }
+ input.addEventListener('input', (e) => {
+      if (input.value.length > 0) {
+       startGame();
+      }
+    });     
+function startGame() {
+      // alert("🎮 Game Started!");
+      // // Your game start logic here
+      levelUp();
+      input.blur();         // Remove focus
+      input.style.display = "none"; // Hide input completely 
+    }
+const Sound1 = new Audio("Click1.wav");
+Sound1.load();
+const Sound2 = new Audio("Click2.wav");
+Sound2.load();
+
+let isStarted = false;
+let body = document.querySelector("body");
+body.addEventListener("keypress",()=>{
+   if(isStarted == false){
+    levelUp();
+    isStarted = true;
+   }
+});
+function levelUp(){
+    levelNum++;
+    userMemory =[];
+    level.innerHTML = `Level : ${levelNum}`;
+    generateColor();
+}
+
+function generateColor(){
+   let num1 = Math.floor(Math.random()*4);
+   switch(num1) {
+       case 0 :
+       color = "red";
+       break;
+       case 1 :
+       color = "green";
+       break;
+       case 2 :
+       color = "yellow";
+       break;
+       case 3 :
+       color = "blue";
+       break;
+       default : 
+       color = "red";
+      }
+      blink(color);
+      gameMemory.push(color); 
+    //   console.log(color);
+}
+
+function blink(color){
+    Sound1.currentTime = 0;
+    Sound1.play();
+    
+    let div = document.getElementById(color);
+    void div.offsetWidth;
+    div.classList.add("blink");
+
+    const animationDuration = 0.1; // seconds
+    const animationDelay = 0.1;     // seconds
+    const iterationCount = 2;
+    const totalTime = (animationDelay + animationDuration) * iterationCount * 1000;
+    setTimeout(()=>{
+        div.classList.remove("blink");
+      },totalTime);
+    // div.addEventListener('animationend', () => {
+    //     div.classList.remove("blink");
+    // }, { once: true });
+}
+
+
+function changeDisplay(){
+    setTimeout(()=>{
+        body.style.backgroundColor = "grey";
+    },1000);
+    level.innerHTML = `Your Score : ${levelNum}`;
+}
+function blink2(color){
+    Sound2.currentTime = 0;
+    Sound2.play();
+
+    let div = document.getElementById(color);
+    div.classList.add("blink");
+    div.addEventListener('animationend', () => {
+            div.classList.remove("blink");
+        }, { once: true });
+
+    let body = document.querySelector("body");
+    body.style.backgroundColor = "red";
+    changeDisplay();
+}
+function checkAns(color,idx){
+    console.log(userMemory);
+    console.log(gameMemory);
+
+    if(userMemory[idx] === gameMemory[idx]){
+            blink(color);
+      if(userMemory.length == gameMemory.length){
+        setTimeout(()=>{
+            levelUp();
+        },1000);
+      }
+    } else {
+        blink2(color); 
+        setTimeout(()=>{
+            window.location = "index.html";
+        },1000);  
+    }
+}
+
+let divList = document.querySelectorAll(".container div");
+for(let div of divList ){
+div.addEventListener("click",function(event){
+   let btn = event.target;
+   let userColor = btn.getAttribute("id");
+     userMemory.push(userColor);
+     checkAns(userColor,userMemory.length-1);
+})};
